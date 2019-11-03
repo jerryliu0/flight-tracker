767237a9c04d5ce1d19413b5bb1a79c8e50e68b6
Jerry Liu <jerry@Jerrys-MacBook-Pro.local>
2019-11-02 22:23:27 -0500

    Updates

---
+++ precache-manifest.d5ad877f6a7980f4ffc7e31da614e184.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d549e5171afd3af903eca23ceb3fdff5",
+    "url": "/flight-tracker/index.html"
+  },
+  {
+    "revision": "9dbc70557d696d8af330",
+    "url": "/flight-tracker/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0deb9ba368445873d7cb",
+    "url": "/flight-tracker/static/js/2.ddeb818d.chunk.js"
+  },
+  {
+    "revision": "9dbc70557d696d8af330",
+    "url": "/flight-tracker/static/js/main.45e366fa.chunk.js"
+  },
+  {
+    "revision": "4a65c4ea841eac6a9b4d",
+    "url": "/flight-tracker/static/js/runtime-main.494c3f97.js"
+  },
+  {
+    "revision": "3b85b920ad758562347e202bbdb6e761",
+    "url": "/flight-tracker/static/media/321.3b85b920.png"
+  },
+  {
+    "revision": "57f5c74e67932f79085e4643258757b2",
+    "url": "/flight-tracker/static/media/738.57f5c74e.png"
+  },
+  {
+    "revision": "3f64ecc576205c6ef5bbd9c0ec21739f",
+    "url": "/flight-tracker/static/media/757.3f64ecc5.png"
+  }
+]);
